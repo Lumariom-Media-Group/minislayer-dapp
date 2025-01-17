edb9e3a76b45581f69c298fa01f94f4fbb3479f8
Nischal Bista <nischalbista0@gmail.com>
2025-01-17 10:51:57 +0545

    Change calculator back to dropdown

--- app/calculator/page.tsx
+++ app/calculator/page.tsx
@@ -1,18 +1,25 @@
 "use client";
 import { getMintAndRedeemPrice } from "@/constants";
 import { useState } from "react";
+import { FaChevronDown } from "react-icons/fa6";
 
 export default function Calculator() {
+  const [currentLevel, setCurrentLevel] = useState(3);
+  const [mintAmount, setMintAmount] = useState(0);
+  const [selectedCalculator, setSelectedCalculator] = useState<
+    "Mint Calculator" | "Redeem Calculator" | ""
+  >("");
 
-  const [currentLevel,setCurrentLevel] = useState(3);
-  const [mintAmount,setMintAmount] = useState(0);
-  const [activeTab, setActiveTab] = useState<"MINT" | "REDEEM">("MINT");
+  const totalAmount =
+    selectedCalculator == "Mint Calculator"
+      ? mintAmount * getMintAndRedeemPrice(currentLevel).mintPrice
+      : mintAmount * getMintAndRedeemPrice(currentLevel).redeemPrice;
 
-
-  const totalAmount =(activeTab == "MINT" ? (mintAmount * getMintAndRedeemPrice(currentLevel).mintPrice) : mintAmount * getMintAndRedeemPrice(currentLevel).redeemPrice)  ;
-
-  function getAmountWithTax(amount: number, activeTab: "MINT" | "REDEEM"): number {
-    if (activeTab === "REDEEM") {
+  function getAmountWithTax(
+    amount: number,
+    selectedCalculator: "Mint Calculator" | "Redeem Calculator" | ""
+  ): number {
+    if (selectedCalculator === "Redeem Calculator") {
       // Subtract 5% from the amount
       return amount * 0.95;
     } else {
@@ -20,9 +27,13 @@ export default function Calculator() {
       return amount * 1.2;
     }
   }
-  
 
+  const [isOpen, setIsOpen] = useState(false);
 
+  const handleSelect = (value: "Redeem Calculator" | "Mint Calculator") => {
+    setSelectedCalculator(value);
+    setIsOpen(false);
+  };
 
   return (
     <div className="min-h-[calc(100dvh-115.963px-88px)] overflow-hidden text-text overflow-x-hidden px-4 sm:px-8 py-10 sm:py-14 lg:py-24">
@@ -32,22 +43,35 @@ export default function Calculator() {
             Mini Slayer Calculator
           </h1>
 
-          {/* Tabs */}
-          <div className="flex gap-4 sm:gap-6 justify-center mb-6">
-            {["MINT", "REDEEM"].map((tab) => (
-              <button
-                key={tab}
-                //@ts-ignore
-                onClick={() => setActiveTab(tab)}
-                className={`px-4 py-2 sm:px-6 sm:py-3 text-sm sm:text-lg font-medium rounded-lg transition-colors ${
-                  activeTab === tab
-                    ? "bg-primary text-white"
-                    : "bg-background text-gray hover:bg-primary-light hover:text-white"
+          <div className="w-full relative">
+            <button
+              onClick={() => setIsOpen(!isOpen)}
+              className="w-full shadow-insetDarkGlow outline-none flex justify-between items-center gap-4 bg-primary text-white font-medium px-4 py-2.5 sm:px-5 sm:py-3 sm:text-xl rounded-lg transition-all hover:bg-primary-light focus:outline-none"
+            >
+              {selectedCalculator ? selectedCalculator : "Select Calculator"}
+              <FaChevronDown
+                className={`transform transition-transform ${
+                  isOpen ? "rotate-180" : ""
                 }`}
-              >
-                {tab}
-              </button>
-            ))}
+              />
+            </button>
+            {isOpen && (
+              <div className="absolute mt-2 shadow-primary shadow-sm bg-background text-text rounded-lg w-full z-10">
+                <button
+                  className="block w-full text-left px-4 py-2.5 sm:py-3 sm:px-5 text-base font-medium rounded-lg transition-colors hover:bg-primary"
+                  onClick={() => handleSelect("Mint Calculator")}
+                >
+                  Mint Calculator
+                </button>
+
+                <button
+                  className="block w-full text-left px-4 py-2.5 sm:py-3 sm:px-5 text-base font-medium rounded-lg transition-colors hover:bg-primary"
+                  onClick={() => handleSelect("Redeem Calculator")}
+                >
+                  Redeem Calculator
+                </button>
+              </div>
+            )}
           </div>
 
           <div className="flex flex-col gap-2 sm:gap-2.5">
@@ -65,10 +89,9 @@ export default function Calculator() {
             <input
               type="number"
               id="labelInput"
-              onChange={(e)=>{
-                if(!isNaN(Number(e.target.value))){
+              onChange={(e) => {
+                if (!isNaN(Number(e.target.value))) {
                   setCurrentLevel(Number(e.target.value));
-
                 }
               }}
               placeholder="0"
@@ -80,7 +103,9 @@ export default function Calculator() {
             <div className="flex flex-col gap-2 sm:gap-2.5">
               <div className="sm:text-lg">
                 <label htmlFor="tokensInput" className="font-medium text-gray">
-                  {activeTab === "MINT"
+                  {selectedCalculator === ""
+                    ? "Tokens Redeem/mint-able"
+                    : selectedCalculator === "Mint Calculator"
                     ? "Tokens Mint-able"
                     : "Tokens Redeemable"}
                 </label>
@@ -109,7 +134,13 @@ export default function Calculator() {
                 id="slayerPriceInput"
                 placeholder="-"
                 disabled
-                value={currentLevel > 0 ? (activeTab == "MINT" ? getMintAndRedeemPrice(currentLevel).mintPrice : getMintAndRedeemPrice(currentLevel).redeemPrice) :"-"}
+                value={
+                  currentLevel > 0
+                    ? selectedCalculator == "Mint Calculator"
+                      ? getMintAndRedeemPrice(currentLevel).mintPrice
+                      : getMintAndRedeemPrice(currentLevel).redeemPrice
+                    : "-"
+                }
                 className="bg-background outline-none placeholder:text-gray-500 text-xl sm:text-2xl font-medium px-4 py-3 md:px-5 md:py-4 rounded-lg shadow-insetDarkGlow border-2 border-transparent transition-all duration-300 focus:border-primary"
               />
             </div>
@@ -128,10 +159,10 @@ export default function Calculator() {
               type="number"
               min={1}
               max={10}
-              onChange={(e)=>{
+              onChange={(e) => {
                 const number = Number(e.target.value);
-                if(!isNaN(number)){
-                  setMintAmount(number)
+                if (!isNaN(number)) {
+                  setMintAmount(number);
                 }
               }}
               id="mintAmountInput"
@@ -143,34 +174,55 @@ export default function Calculator() {
           <div className="flex flex-col gap-2 sm:gap-2.5">
             <div className="sm:text-lg">
               <p className="font-medium text-gray">
-                {activeTab === "MINT"
+                {selectedCalculator === ""
+                  ? "Total USDV spent/received"
+                  : selectedCalculator === "Mint Calculator"
                   ? "Total USDV spent"
                   : "Total USDV received"}
               </p>
             </div>
-            <div className="bg-background flex flex-col gap-1 font-medium px-4 py-3 md:px-5 md:py-4 rounded-lg shadow-insetDarkGlow">
-              <div className="flex items-center gap-1 font-bold text-base sm:text-lg">
-                <span>{totalAmount} </span>
-                <span className={"text-red"}>{activeTab == "MINT" ? "+20% Tax" : "-5% Tax"}</span>
+            {selectedCalculator === "" ? (
+              <div className="bg-background flex flex-col gap-1 font-medium px-4 py-3 text-2xl sm:text-4xl md:px-5 md:py-4 rounded-lg shadow-insetDarkGlow">
+                -
               </div>
-              <div className="font-semibold text-blue text-3xl sm:text-4xl">
-                {getAmountWithTax(totalAmount,activeTab)} USDV
+            ) : (
+              <div className="bg-background flex flex-col gap-1 font-medium px-4 py-3 md:px-5 md:py-4 rounded-lg shadow-insetDarkGlow">
+                <div className="flex items-center gap-1 font-bold text-base sm:text-lg">
+                  <span>{totalAmount}</span>
+                  <span className="text-red">
+                    {selectedCalculator == "Mint Calculator"
+                      ? "+20% Tax"
+                      : "-5% Tax"}
+                  </span>
+                </div>
+                <div className="font-semibold text-blue text-3xl sm:text-4xl">
+                  {getAmountWithTax(totalAmount, selectedCalculator)} USDV
+                </div>
               </div>
-            </div>
+            )}
           </div>
 
           <div className="flex flex-col gap-2 sm:gap-2.5">
             <div className="sm:text-lg">
               <p className="font-medium text-gray">Total USDT cost value:</p>
             </div>
-            <div className="bg-background flex flex-col gap-1 font-medium px-4 py-3 md:px-5 md:py-4 rounded-lg shadow-insetDarkGlow">
-              <div className="font-semibold text-base sm:text-lg">
-                <span>{getAmountWithTax(totalAmount,activeTab)} USDV x 1.25</span>
+            {selectedCalculator === "" ? (
+              <div className="bg-background flex flex-col gap-1 font-medium px-4 py-3 text-2xl sm:text-4xl md:px-5 md:py-4 rounded-lg shadow-insetDarkGlow">
+                -
               </div>
-              <div className="font-bold text-green text-3xl sm:text-4xl">
-                $ {getAmountWithTax(totalAmount,activeTab) * 1.25}
+            ) : (
+              <div className="bg-background flex flex-col gap-1 font-medium px-4 py-3 md:px-5 md:py-4 rounded-lg shadow-insetDarkGlow">
+                <div className="font-semibold text-base sm:text-lg">
+                  <span>
+                    {getAmountWithTax(totalAmount, selectedCalculator)} USDV x
+                    1.25
+                  </span>
+                </div>
+                <div className="font-bold text-green text-3xl sm:text-4xl">
+                  $ {getAmountWithTax(totalAmount, selectedCalculator) * 1.25}
+                </div>
               </div>
-            </div>
+            )}
           </div>
         </div>
       </div>
