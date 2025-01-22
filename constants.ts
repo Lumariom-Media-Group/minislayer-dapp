6502875feb46248228d16e355be9a89915b6d92c
dineshtiwari69 <210343@softwarica.edu.np>
2025-01-22 23:31:18 +0545

    Change Contract Address

--- constants.ts
+++ constants.ts
@@ -1,7 +1,7 @@
 
 export const USDV="0xB6434db1Ff5549Cb8be98156bC65a7743084AA97";
 export const USDV_DECIMALS=6;
-export const MINI_SLAYER ="0x8908253Bf92bdDC20ea9d3F3E1C5E1d7e1431D8C";
+export const MINI_SLAYER ="0xEcfBd053801f7c2844CbEc0E8b9d09970528Fa00";
 export const MINI_SLAYER_DECIMALS=6;
 export function getMintAndRedeemPrice(level:number){
     return {mintPrice:level,redeemPrice:level-1};
