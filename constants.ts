6ecb9aee5908226e9254192c62ef44114176bd9a
dineshtiwari69 <210343@softwarica.edu.np>
2025-01-25 00:33:43 +0545

    Change Addres for Mini slayer

--- constants.ts
+++ constants.ts
@@ -1,7 +1,7 @@
 
 export const USDV="0xB6434db1Ff5549Cb8be98156bC65a7743084AA97";
 export const USDV_DECIMALS=6;
-export const MINI_SLAYER ="0xEcfBd053801f7c2844CbEc0E8b9d09970528Fa00";
+export const MINI_SLAYER ="0x4130C9CBD1502AC1B17AfeE20c9E620937a66c0b";
 export const MINI_SLAYER_DECIMALS=6;
 export function getMintAndRedeemPrice(level:number){
     return {mintPrice:level,redeemPrice:level-1};
