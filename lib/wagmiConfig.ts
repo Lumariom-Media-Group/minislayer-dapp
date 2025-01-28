1445b08160172ef17bbbcccafcab6dcf0200128e
dineshtiwari69 <210343@softwarica.edu.np>
2025-01-28 22:12:35 +0545

    Add rpc

--- lib/wagmiConfig.ts
+++ lib/wagmiConfig.ts
@@ -6,6 +6,7 @@ import {
     getDefaultConfig,
 
 } from '@rainbow-me/rainbowkit';
+import { http,fallback } from '@wagmi/core'
 
 import {
 
@@ -17,6 +18,7 @@ import {
 export const wagmiConfig = getDefaultConfig({
     appName: 'My RainbowKit App',
     projectId: '876dfdad5fe2328754adbfc08c7aaf4f',
+    transports:{[polygon.id]:fallback([http("https://polygon-mainnet.infura.io/v3/9ff2374d8b41416284fb36376dc72b69"),http("https://polygon-mainnet.infura.io/v3/3545d87e204f4141856095aa9c1a1da1")])},
     chains: [polygon],
     ssr: true, // If your dApp uses server side rendering (SSR)
 });
