7c4c0ee4194e623bf4d5006362ba092f47882a4a
dineshtiwari69 <210343@softwarica.edu.np>
2025-01-31 16:23:56 +0545

    Change contract

--- constants.ts
+++ constants.ts
@@ -1,7 +1,7 @@
 
 export const USDV="0xdAc9856128C93931F4cbdffEcfA1B5cA801AE16F";
 export const USDV_DECIMALS=6;
-export const MINI_SLAYER ="0x28071CF86A6D405AbA74A8738e4770Ee2de5242e";
+export const MINI_SLAYER ="0x446B991F252C1a39c428e2C6BeE590Fd69C8f947";
 export const MINI_SLAYER_DECIMALS=6;
 export function getMintAndRedeemPrice(level:number){
     return {mintPrice:level,redeemPrice:level-1};
