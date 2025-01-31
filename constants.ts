b04178ba0f7ee62e022366cddb3abb10bc5e0423
dineshtiwari69 <210343@softwarica.edu.np>
2025-01-31 18:07:13 +0545

    Change address

--- constants.ts
+++ constants.ts
@@ -1,7 +1,7 @@
 
 export const USDV="0xdAc9856128C93931F4cbdffEcfA1B5cA801AE16F";
 export const USDV_DECIMALS=6;
-export const MINI_SLAYER ="0x7B90aD56485AC62c717a34770D471602ec3297F8";
+export const MINI_SLAYER ="0xD1686c99f6d07A9d17B412EE7C0BFf8AC40EBe36";
 export const MINI_SLAYER_DECIMALS=6;
 export function getMintAndRedeemPrice(level:number){
     return {mintPrice:level,redeemPrice:level-1};
