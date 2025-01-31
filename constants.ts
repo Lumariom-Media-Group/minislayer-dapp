779d3c4e9ea28deea7d518f72954155e92bce4eb
dineshtiwari69 <210343@softwarica.edu.np>
2025-02-01 00:29:57 +0545

    Hook Dapp with prod contract

--- constants.ts
+++ constants.ts
@@ -1,7 +1,7 @@
 
 export const USDV="0xdAc9856128C93931F4cbdffEcfA1B5cA801AE16F";
 export const USDV_DECIMALS=6;
-export const MINI_SLAYER ="0x09098a73d9cBDb41FaDa4Ef8E8E16A52c4372aA6";
+export const MINI_SLAYER ="0x605097BC2a9ff3322a2F4daF66d48df7d1504DB4";
 export const MINI_SLAYER_DECIMALS=6;
 export function getMintAndRedeemPrice(level:number){
     return {mintPrice:level,redeemPrice:level-1};
